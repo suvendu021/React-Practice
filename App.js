8772705ec81edd697dd683b5bea7e96102533a0b
suvendu021 <suvendukumarsahoo2002@gmail.com>
2024-01-18 19:54:08 +0530

    day 2 (Explore different folders importance)

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "hello, i'm root !!!"
+  "hello, i'm root 😢 🫨!!!"
 );
 // console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
